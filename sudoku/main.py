325be861783de98f8d54c9a1f2cef215e74dbe6a
notgitika <gitika.jha@somaiya.edu>
2020-12-16 20:32:55 +0530

    Implemented backtracking algo on CLI

--- sudoku/main.py
+++ sudoku/main.py
@@ -13,12 +13,58 @@ BOARD = [
 ]
 
 
+def solve(bo):
+    """backtracking algo"""
+
+    # base case
+    find = empty_space(bo)
+    if not find:
+        return True
+    else:
+        row, col = find
+
+    for i in range(1, 10):
+        if valid(bo, i, (row, col)):
+            bo[row][col] = i
+
+            if solve(bo):
+                return True
+
+            bo[row][col] = 0
+
+    return False
+
+
+def valid(bo, num, pos):
+    """To check if the num we entered is okay to do so"""
+    # for row
+    for i in range(len(bo[0])):
+        if bo[pos[0]][i] == num and pos[1] != i:
+            return False
+
+    # for column
+    for i in range(len(bo)):
+        if bo[i][pos[1]] == num and pos[0] != i:
+            return False
+
+    # for a certain grid/box
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
+
+    for i in range(box_y * 3, box_y * 3 + 3):
+        for j in range(box_x * 3, box_x * 3 + 3):
+            if bo[i][j] == num and (i, j) != pos:
+                return False
+
+    return True
+
+
 def display_board(bo):
     """Printing the sample board in a formatted way on CLI"""
 
     for i in range(len(bo)):
         if i % 3 == 0 and i != 0:   # end of 3 rows, print a straight line
-            print("- - - - - - - - - - - - - - -")
+            print("- - - - - - - - - - - -")
 
         for j in range(len(bo[0])):  # for each element in row
             if j % 3 == 0 and j != 0:   # end of 3 elements, print a straight line
@@ -36,6 +82,10 @@ def empty_space(bo):
         for j in range(len(bo[0])):
             if bo[i][j] == 0:
                 return(i, j)  # row and column position of that element
+    return None
 
 
-return None
+display_board(BOARD)
+print("Solution is:")
+solve(BOARD)
+display_board(BOARD)
